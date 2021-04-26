b9adcecb550e97aaeb9eb699b1c117df937267a2
Trevor Suter <Trevorsuter@icloud.com>
2021-04-26 11:27:25 -0600

    took out unnecessary local variable in format

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -8,17 +8,17 @@ class Api::V1::SalariesController < ApplicationController
   end
 
   def formatted_output(forecast, salaries)
-    output = { data: {
-                id: nil,
-                type: "salaries",
-                attributes: {
-                  destination: params[:destination],
-                  forecast: {
-                    summary: forecast['conditions'],
-                    temperature: forecast['temperature']
-                  },
-                  salaries: salaries
-                } 
+    { data: {
+        id: nil,
+        type: "salaries",
+        attributes: {
+          destination: params[:destination],
+          forecast: {
+            summary: forecast['conditions'],
+            temperature: forecast['temperature']
+          },
+          salaries: salaries
+        } 
     }}.to_json
   end
 end
